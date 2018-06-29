04f7ba1c8d98a4858ac92d299e8cbdd9d330edbe
1412557 <kysydragony@gmail.com>
2018-06-29 17:16:48 +0700

    Create sample datas via seed method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |x|
+	Blog.create!(
+		title: "My Blog Post #{x}",
+		body: "hello this is a paragraph: adsdasdasdasd
+		asdasdasf asdjkasdm nasd asdjaskdjaskd asdjkhasjkdas
+		asdjkaksdjajksbnjkfaskdjasd
+		asjkdkasjd"
+	)
+end
+
+5.times do |x|
+	Skill.create!(
+		title:"Rails #{x}",
+		percent_utilized: 15
+	)
+end
+
+9.times do |x|
+	Portfolio.create!(
+		title: "Porfolio title: #{x}",
+		subtitle: "My great service",
+		body: "this is a very large body",
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200"
+	)
+end
